6c574cb615a4f9920a57e46815a8e25ed1d8a81f
NShalom2 <136999298+NShalom2@users.noreply.github.com>
2024-03-23 20:36:35 -0400

    Added some Functionality to the checkboxes

--- Assignment 2 Files/js/script.js
+++ Assignment 2 Files/js/script.js
@@ -1,15 +1,28 @@
-const enlargeImages = document.querySelectorAll(".base-liquid-images");
 const body = document.querySelector("body");
 
-enlargeImages.forEach(image => {
-    image.addEventListener("mouseenter", function(){
-        this.style.height = "150px";
-        this.borderRadius = "0";
-    });
-});
+document.getElementById("submit").addEventListener("click", function(){
 
-enlargeImages.forEach(image => {
-    image.addEventListener("mouseleave", function(){
-        this.style.height = "90px";
-    });
+
+    event.preventDefault();
+
+    let selectedRadioValue = "";
+    let selectedCheckValues = [];
+
+    let radioButtons = document.getElementsByName("base-liquid");
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            selectedRadioValue = radioButtons[i].value;
+            break;
+        }
+    }
+
+    let checkBoxes = document.getElementsByName("fruit");
+    for (let j = 0; j < checkBoxes.length; j++) {
+        if (checkBoxes[j].checked) {
+            selectedCheckValues.push(checkBoxes[j].value);
+        }
+    }
+
+    console.log("Selected radio value: " + selectedRadioValue);
+    console.log("Selected checkbox values: " + selectedCheckValues);
 });
