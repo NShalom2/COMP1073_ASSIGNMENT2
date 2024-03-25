0cb8584664420a15ba72de672490c9e6f52a27fe
NShalom2 <136999298+NShalom2@users.noreply.github.com>
2024-03-25 11:14:33 -0400

    Added some functionality to the check buttons

--- Assignment 2 Files/js/script.js
+++ Assignment 2 Files/js/script.js
@@ -1,17 +1,19 @@
-const body = document.querySelector("body");
+let finalChoices = document.querySelector(".final_choices")
 
 document.getElementById("submit").addEventListener("click", function(){
 
 
     event.preventDefault();
 
-    let selectedRadioValue = "";
-    let selectedCheckValues = [];
+    let selectedBaseLiquid = "";
+    let selectedFruits = [];
+    let selectedSweeteners = [];
+    let selectedThickener = [];
 
-    let radioButtons = document.getElementsByName("base-liquid");
-    for (let i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked) {
-            selectedRadioValue = radioButtons[i].value;
+    let baseLiquid = document.getElementsByName("base-liquid");
+    for (let i = 0; i < baseLiquid.length; i++) {
+        if (baseLiquid[i].checked) {
+            selectedBaseLiquid = baseLiquid[i].value;
             break;
         }
     }
@@ -19,10 +21,35 @@ document.getElementById("submit").addEventListener("click", function(){
     let checkBoxes = document.getElementsByName("fruit");
     for (let j = 0; j < checkBoxes.length; j++) {
         if (checkBoxes[j].checked) {
-            selectedCheckValues.push(checkBoxes[j].value);
+            selectedFruits.push(checkBoxes[j].value);
         }
     }
 
-    console.log("Selected radio value: " + selectedRadioValue);
-    console.log("Selected checkbox values: " + selectedCheckValues);
+    let sweetenerCheckBoxes = document.getElementsByName("sweetener");
+    for (let k = 0; k < sweetenerCheckBoxes.length; k++) {
+        if (sweetenerCheckBoxes[k].checked) {
+            selectedSweeteners.push(sweetenerCheckBoxes[k].value);
+        }
+    }
+
+    let thickener = document.getElementsByName("thickener");
+    for (let l = 0; l < thickener.length; l++) {
+        if (thickener[l].checked) {
+            selectedThickener = thickener[l].value;
+            break;
+        }
+    }
+
+    console.log("Selected Base Liquid: " + selectedBaseLiquid);
+    console.log("Selected Fruits and Vegetables: " + selectedFruits);
+    console.log("Selected Sweetener: " + selectedSweeteners);
+    console.log("Selected Thickener: " + selectedThickener);
+
+    if (selectedSweeteners.length === 0 && !selectedThickener) {
+        finalChoices.textContent = "You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables.";
+        console.log("You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables.");
+    } else if (selectedSweeteners.length > 0 && !selectedThickener){
+        finalChoices.textContent = "You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables, " + selectedSweeteners + " as the sweeteners and " + selectedThickener + " as the thickener.";
+        console.log("You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables, " + selectedSweeteners + " as the sweeteners and " + selectedThickener + " as the thickener.");
+    }
 });
