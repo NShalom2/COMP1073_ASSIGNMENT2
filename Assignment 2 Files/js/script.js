4340fc89bfb6c0c7a726cdb7547a5b7c4128afb1
NShalom2 <136999298+NShalom2@users.noreply.github.com>
2024-03-25 17:22:56 -0400

    Added further functionality for the website

--- Assignment 2 Files/js/script.js
+++ Assignment 2 Files/js/script.js
@@ -1,10 +1,11 @@
-let finalChoices = document.querySelector(".final_choices")
+const finalChoices = document.querySelector(".final_choices");
+const baseLiquidsError = document.querySelector(".base_liquids_error");
+const fruitsError = document.querySelector(".fruits_error");
 
+let userInput;
 document.getElementById("submit").addEventListener("click", function(){
 
-
     event.preventDefault();
-
     let selectedBaseLiquid = "";
     let selectedFruits = [];
     let selectedSweeteners = [];
@@ -13,15 +14,17 @@ document.getElementById("submit").addEventListener("click", function(){
     let baseLiquid = document.getElementsByName("base-liquid");
     for (let i = 0; i < baseLiquid.length; i++) {
         if (baseLiquid[i].checked) {
+            baseLiquidsError.remove();
             selectedBaseLiquid = baseLiquid[i].value;
             break;
         }
     }
 
-    let checkBoxes = document.getElementsByName("fruit");
-    for (let j = 0; j < checkBoxes.length; j++) {
-        if (checkBoxes[j].checked) {
-            selectedFruits.push(checkBoxes[j].value);
+    let fruits = document.getElementsByName("fruit");
+    for (let j = 0; j < fruits.length; j++) {
+        if (fruits[j].checked) {
+            fruitsError.remove();
+            selectedFruits.push(fruits[j].value);
         }
     }
 
@@ -40,16 +43,29 @@ document.getElementById("submit").addEventListener("click", function(){
         }
     }
 
-    console.log("Selected Base Liquid: " + selectedBaseLiquid);
-    console.log("Selected Fruits and Vegetables: " + selectedFruits);
-    console.log("Selected Sweetener: " + selectedSweeteners);
-    console.log("Selected Thickener: " + selectedThickener);
-
-    if (selectedSweeteners.length === 0 && !selectedThickener) {
-        finalChoices.textContent = "You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables.";
-        console.log("You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables.");
-    } else if (selectedSweeteners.length > 0 && !selectedThickener){
-        finalChoices.textContent = "You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables, " + selectedSweeteners + " as the sweeteners and " + selectedThickener + " as the thickener.";
-        console.log("You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits and vegetables, " + selectedSweeteners + " as the sweeteners and " + selectedThickener + " as the thickener.");
+    if (!selectedBaseLiquid){
+        baseLiquidsError.textContent = "* You must select a base liquid";
+    } else if(selectedFruits.length < 1) {
+        fruitsError.textContent = "* You must select one or more fruits or vegetables";
+    } else {
+        if (selectedSweeteners.length === 0 + !selectedThickener){
+            userInput = prompt("You have selected " + selectedBaseLiquid + " as the base liquid and " + selectedFruits + " as the fruits or vegetables and no sweeteners or thickeners. Type 'Yes' if all looks good.");
+            userInputFunction(userInput)
+        } else if (selectedSweeteners.length > 0 + selectedThickener != null){
+            userInput = prompt("You have selected " + selectedBaseLiquid + " as the base liquid, " + selectedFruits + " as the fruits or vegetables, " + selectedSweeteners + " as the sweetener(s) and " + selectedThickener + " as  the thickener. Type 'Yes' if all looks good.");
+            userInputFunction(userInput)
+        } else if (selectedSweeteners.length > 0 + !selectedThickener){
+            userInput = prompt("You have selected " + selectedBaseLiquid + " as the base liquid, " + selectedFruits + " as the fruits or vegetables, " + selectedSweeteners + " as the sweetener(s) and no thickener. Type 'Yes' if all looks good.");
+            userInputFunction(userInput)
+        } else if (selectedSweeteners.length === 0 + selectedThickener != null){
+            userInput = prompt("You have selected " + selectedBaseLiquid + " as the base liquid, " + selectedFruits + " as the fruits or vegetables, no sweetener(s) and " + selectedThickener + " as  the thickener. Type 'Yes' if all looks good.");
+            userInputFunction(userInput)
+        }
     }
-});
+
+    function userInputFunction(value){
+        if (value.toLowerCase()[0] === 'y') {
+            window.location.href = "smoothie.html";
+        }
+    }
+})
