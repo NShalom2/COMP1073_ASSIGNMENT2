7cb46e2430cfc1a31e6c4382faca98e6e5f509f5
NShalom2 <136999298+NShalom2@users.noreply.github.com>
2024-04-19 14:13:25 -0400

    Updated Code

--- COMP1073_ASSIGNMENT_3/js/script.js
+++ COMP1073_ASSIGNMENT_3/js/script.js
@@ -17,15 +17,17 @@ function submit(event){
 }
 
 function printResults(json){
+    console.log(json);
     const newSection = document.createElement("article");
-    while (newSection.firstChild) {
-        newSection.removeChild(newSection.firstChild);
-    }
+    while (section.firstChild) {
+        section.removeChild(section.firstChild);
+    };
     for(let i=0; i<json.length; i++) {
         const countryName = document.createElement("h1");
         const countryCapital = document.createElement("p");
         const countryFlag = document.createElement("img");
         const countryContinent = document.createElement("p");
+        const countryPopulation = document.createElement("p");
 
         countryName.textContent = `${searchString.value}`;
 
@@ -33,11 +35,14 @@ function printResults(json){
 
         countryContinent.textContent = `Country Continent: ${json[i].continents}`
 
+        countryPopulation.textContent = `Country Population: ${json[i].population}`
+
         countryFlag.src =`${json[i].flags.png}`;
         countryFlag.alt = `${searchString.value} Flag`;
 
         newSection.appendChild(countryName);
         newSection.appendChild(countryCapital);
+        newSection.appendChild(countryPopulation);
         newSection.appendChild(countryContinent);
         newSection.appendChild(countryFlag);
 
